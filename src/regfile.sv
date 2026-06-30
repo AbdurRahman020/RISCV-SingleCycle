7b6416899c2229fa6e170c9591906341675f5cbe
Abdur Rahman Mirza <a.rahmanmirza20@gmail.com>
2026-06-30 19:20:24 +0500

    fix(regfile): guard x0 write to prevent multi-driver conflict

--- src/regfile.sv
+++ src/regfile.sv
@@ -6,14 +6,15 @@ module regfile(
     output logic  [31:0]      RD1, RD2
 );
 
-    logic [31:0] x [31:0]; //32-bit 32 registers
+    logic [31:0] x [31:0];          // 32-bit 32 registers
 
-    always_ff @(posedge CLK) begin //synchronus write
-        if (WE3) x[A3] <= WD3;
+    always_ff @(posedge CLK) begin  // synchronus write
+        if (WE3 && A3 != 5'b0)
+            x[A3] <= WD3;
     end
 
-    assign x[0] = 0;    //x0 hardwired to 0
-    assign RD1 = x[A1]; //read port 1
-    assign RD2 = x[A2]; //read port 2
+    assign x[0] = 0;                // x0 hardwired to 0
+    assign RD1 = x[A1];             // read port 1
+    assign RD2 = x[A2];             // read port 2
 
 endmodule
